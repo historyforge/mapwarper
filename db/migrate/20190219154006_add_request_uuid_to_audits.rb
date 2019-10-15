be59411202edd6506ac0c2661463822291ab4c43
David Furber <furberd@gmail.com>
2019-10-15 10:23:00 -0400

    remove the temp quickfix

--- db/migrate/20190219154006_add_request_uuid_to_audits.rb
+++ db/migrate/20190219154006_add_request_uuid_to_audits.rb
@@ -1,7 +1,7 @@
 class AddRequestUuidToAudits < ActiveRecord::Migration[4.2]
   def self.up
-    # add_column :audits, :request_uuid, :string
-    # add_index :audits, :request_uuid
+    add_column :audits, :request_uuid, :string
+    add_index :audits, :request_uuid
   end
 
   def self.down
